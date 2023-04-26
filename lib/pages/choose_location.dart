929ddeb3fecdef24929972d9ea1504e5b93e72ce
Mozammalhossainsobug <mozammalruet@gmail.com>
2023-04-26 12:34:44 +0600

    Added show button

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,7 +9,8 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  late String? userInput;
+   String? userInput;
+
   void updateTime(String userInput) async {
 
       WorldTime instance = WorldTime(userInput: userInput);
@@ -31,35 +32,65 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-          padding: EdgeInsets.fromLTRB(20.0,20.0,20.0,20.0),
-        decoration:
-        BoxDecoration(
-          color: Colors.blueAccent,
-          border: Border.all(color: Colors.grey, width: .5),
-          borderRadius: BorderRadius.circular(3),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(2, 2),
-              blurRadius: 5,
-            ),
-          ],
-        ),
-            child: TextField(
-              onChanged: (value) {
+      body:  Column(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+              padding: EdgeInsets.fromLTRB(20.0,20.0,20.0,20.0),
+              decoration:
+              BoxDecoration(
+                color: Colors.blueAccent,
+                border: Border.all(color: Colors.grey, width: .5),
+                borderRadius: BorderRadius.circular(3),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(2, 2),
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child:
+              TextField(
+                onChanged: (value) {
                   userInput = value;
-                  updateTime(userInput!);
-                // assign the value entered by the user to userInput variable
-              },
-              decoration: InputDecoration(
-                labelText : 'Enter City here',
+                  // assign the value entered by the user to userInput variable
+                },
+                decoration: InputDecoration(
+                  labelText : 'Enter City here',
+                ),
               ),
             ),
-    ),
-      ),
-      );
+            Container(
+              margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+              padding: EdgeInsets.fromLTRB(40.0,10.0,40.0,10.0),
+              decoration:
+              BoxDecoration(
+                color: Colors.blueAccent,
+                border: Border.all(color: Colors.grey, width: .5),
+                borderRadius: BorderRadius.circular(3),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(2, 2),
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child:
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white, disabledForegroundColor: Colors.white,
+                ),
+                onPressed: (){
+                  updateTime(userInput!);
+                },
+                child: Text('Show'),
+              )
+            ),
+
+          ],
+        ),
+    );
   }
 }
